dec8c79af9ccee70d077f9d400b28315bb04ed42
Loonyx <278818735+Loonyx1@users.noreply.github.com>
2026-05-10 14:09:34 -0300

    Actualización desde Vídeos/i3dots

--- packages/i3dots/dotfiles/polybar_configs/polybar_underline/launch.sh
+++ packages/i3dots/dotfiles/polybar_configs/polybar_underline/launch.sh
@@ -17,6 +17,6 @@ done
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar i3_bar 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar bottom 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
