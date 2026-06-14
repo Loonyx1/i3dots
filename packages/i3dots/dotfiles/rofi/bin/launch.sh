2a9fea11ec1d8f1e09f632cf44cafd9092169c40
Derszi65g <145814859+Derszi65g@users.noreply.github.com>
2026-06-14 00:09:47 -0500

    limpieza y optimizacion

--- packages/i3dots/dotfiles/rofi/bin/launch.sh
+++ packages/i3dots/dotfiles/rofi/bin/launch.sh
@@ -2,10 +2,13 @@
 # launch.sh - Lanzador de aplicaciones (Type-3 Style-3)
 
 # 1. Obtener la ruta del wallpaper actual
-IMAGE_PATH=$(cat "$HOME/.config/i3/wall" 2>/dev/null)
+IMAGE_PATH=""
+if [[ -f "$HOME/.config/i3/wall" ]]; then
+    read -r IMAGE_PATH < "$HOME/.config/i3/wall"
+fi
 
 # 2. Definir el tema
 THEME="$HOME/.config/rofi/themes/style-3.rasi"
 
 # 3. Ejecutar Rofi con la imagen de fondo dinámica en el inputbar
-rofi -show drun -theme "$THEME" -theme-str "inputbar { background-image: url(\"$IMAGE_PATH\", width); }"
+exec rofi -show drun -theme "$THEME" -theme-str "inputbar { background-image: url(\"$IMAGE_PATH\", width); }"
